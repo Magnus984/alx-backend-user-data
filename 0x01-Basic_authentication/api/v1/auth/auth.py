686de108c58dcf115eb3d7d2bb5fd11ac175e572
Magnus984 <nharnharyhaw984@gmail>
2024-11-11 22:55:40 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,19 @@ class Auth:
     Manage API authentication.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if path requires authentication
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """
+        Gets authorization header field from request
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Gets current user from request
+        """
         return None

d072894026a5c82ba7325a0d28d6e2bb72d14937
Magnus984 <nharnharyhaw984@gmail>
2024-11-12 22:43:10 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic auth module.
 """
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -21,3 +23,22 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             return authorization_header[6:]
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """
+        Decodes base64 authorization header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            res = base64.b64decode(
+                base64_authorization_header,
+                validate=True,
+            )
+            return res.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None

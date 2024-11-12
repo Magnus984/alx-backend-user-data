a360ec82d5a082c91b06972e37e29f886b0ad215
Magnus984 <nharnharyhaw984@gmail>
2024-11-12 18:55:29 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Module for authentication
 """
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -14,7 +15,18 @@ class Auth:
         """
         Checks if path requires authentication
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for excluded_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if excluded_path[-1] == '*':
+                    pattern = '{}.*'.format(excluded_path[0:-1])
+                elif excluded_path[-1] == '/':
+                    pattern = '{}/*'.format(excluded_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(excluded_path[0:-1])
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """

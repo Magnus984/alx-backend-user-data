6542e0580ab55ebc51f6fc28ad05b879f343cdc4
Magnus984 <nharnharyhaw984@gmail>
2024-11-12 23:28:12 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ Basic auth module.
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+import re
 
 
 class BasicAuth(Auth):
@@ -42,3 +43,22 @@ class BasicAuth(Auth):
             return res.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> (str, str):
+        """
+        Extracts user credentials
+        """
+        if isinstance(decoded_base64_authorization_header, str):
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None

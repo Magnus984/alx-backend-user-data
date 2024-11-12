b78c192c0aa449223e8e5315c62d69a8c7d92524
Magnus984 <nharnharyhaw984@gmail>
2024-11-12 23:37:07 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 import base64
 import binascii
 import re
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -62,3 +63,21 @@ class BasicAuth(Auth):
                 password = field_match.group('password')
                 return user, password
         return None, None
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str) -> TypeVar('User'):
+        """
+        Gets user object from credentials
+        """
+        if isinstance(user_email, str) and isinstance(user_pwd, str):
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None

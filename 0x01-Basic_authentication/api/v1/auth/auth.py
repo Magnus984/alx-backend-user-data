7083dbd9efa609a710453aa180e5ae76472c0ba5
Magnus984 <nharnharyhaw984@gmail>
2024-11-12 19:44:57 +0000

    add

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -32,7 +32,9 @@ class Auth:
         """
         Gets authorization header field from request
         """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """

6156edca3d626c3fcffa3db08949659c600c533f
Magnus984 <nharnharyhaw984@gmail>
2024-11-14 20:55:22 +0000

    add

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Module for authentication
+"""
+from flask import request
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    """
+    Manage API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if path requires authentication
+        """
+        if path is not None and excluded_paths is not None:
+            for excluded_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if excluded_path[-1] == '*':
+                    pattern = '{}.*'.format(excluded_path[0:-1])
+                elif excluded_path[-1] == '/':
+                    pattern = '{}/*'.format(excluded_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(excluded_path[0:-1])
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Gets authorization header field from request
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Gets current user from request
+        """
+        return None

4b89167574d50f6e47d2d11f0d9609b1eb9b251f
Magnus <nharnharyhaw984@gmail.com>
2024-11-24 18:46:34 +0000

    Added sequence id

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,7 @@
 Module for user model.
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Sequence
 
 Base = declarative_base()
 
@@ -14,7 +14,7 @@ class User(Base):
     """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     email = Column(String(length=250), nullable=False)
     hashed_password = Column(String(length=250), nullable=False)
     session_id = Column(String(length=250), nullable=True)

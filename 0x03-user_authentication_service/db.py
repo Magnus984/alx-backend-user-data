67a458423df875ff93bc8929bad831af295707f6
Magnus <nharnharyhaw984@gmail.com>
2024-11-24 21:10:29 +0000

    fix annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -43,7 +43,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs: dict) -> Union[list, None]:
+    def find_user_by(self, **kwargs: dict) -> Union[User, None]:
         """Finds user from database
         """
         for key in kwargs.keys():

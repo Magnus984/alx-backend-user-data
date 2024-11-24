95d71d483d942022c0e7a19ecd7f53cbb0db7070
Magnus <nharnharyhaw984@gmail.com>
2024-11-24 20:53:14 +0000

    added find_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from typing import Optional
 
 from user import Base, User
 
@@ -16,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -39,3 +42,16 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: dict) -> Optional[User]:
+        """Finds user from database
+        """
+        for key in kwargs.keys():
+            if key == "email":
+                row = self._session.query(User).filter(
+                    User.email == kwargs.get(key)
+                    ).first()
+                if row is None:
+                    raise NoResultFound
+                return row
+        raise InvalidRequestError

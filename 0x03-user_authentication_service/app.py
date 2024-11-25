4e61c3d663ab6b4dd1d1b125d8af6c2971f104bf
Magnus <nharnharyhaw984@gmail.com>
2024-11-25 13:25:21 +0000

    define routes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
@@ -13,6 +15,26 @@ def home() -> str:
       - JSON payload
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def register_user():
+    """ POST /users
+    Return:
+      - JSON payload
+    """
+    email = request.form["email"]
+    password = request.form["password"]
+    if email and password:
+        try:
+            user = auth.register_user(email, password)
+            return jsonify({
+                "email": f"{email}", "message": "user created" 
+            })
+        except ValueError:
+            return jsonify({
+                "message": "email already registered"
+            }), 400
+
 
 
 if __name__ == "__main__":

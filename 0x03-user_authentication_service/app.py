01edf452ac1874828e1605216bc2e08d268f8f04
Magnus <nharnharyhaw984@gmail.com>
2024-11-25 17:54:11 +0000

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def login():
                 "email": f"{email}", "message": "logged in"
             })
         abort(401)
-            
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

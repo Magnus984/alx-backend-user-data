21087036f54d5ee3fef3b85c723b2107befa3d40
Magnus <nharnharyhaw984@gmail.com>
2024-11-25 15:58:44 +0000

    add: private method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -45,3 +46,8 @@ class Auth:
             return bcrypt.checkpw(password_to_byte, password_hashed)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Returns the string representation of a new UUID
+        """
+        return str(uuid.uuid4())

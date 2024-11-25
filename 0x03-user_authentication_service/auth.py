08fa0f50c639f7986fbca67718b48c373ae1f7b6
Magnus <nharnharyhaw984@gmail.com>
2024-11-25 15:47:10 +0000

    add: valid_login method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,14 @@ class Auth:
             password_hashed = _hash_password(password)
             new_user = self._db.add_user(email, password_hashed)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if login is valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            password_to_byte = password.encode('utf-8')
+            password_hashed = _hash_password(password)
+            return bcrypt.checkpw(password_to_byte, password_hashed)
+        except NoResultFound:
+            return False

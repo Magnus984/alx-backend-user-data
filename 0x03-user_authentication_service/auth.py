3617d904b5ce7c76195f116e4adf2dd3332d1ea3
Magnus <nharnharyhaw984@gmail.com>
2024-11-25 16:19:09 +0000

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,6 +17,12 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """Returns the string representation of a new UUID
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -46,8 +52,3 @@ class Auth:
             return bcrypt.checkpw(password_to_byte, password_hashed)
         except NoResultFound:
             return False
-
-    def _generate_uuid(self) -> str:
-        """Returns the string representation of a new UUID
-        """
-        return str(uuid.uuid4())
